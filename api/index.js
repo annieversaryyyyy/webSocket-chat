a74a43524685c76e08f2a1d8d70d94012fdde4a9
annieversaryyyyy <kduzkdkuzkdkd@gmail.com>
2026-03-02 17:18:34 +0600

    feat(chat): add WebSocket connection, message sending and broadcasting

--- api/index.js
+++ api/index.js
@@ -1,5 +1,47 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const crypto = require("crypto");
 
 require("express-ws")(app);
+const port = 8000;
+
+app.use(cors());
+const activeConnections = {};
+
+app.ws("/chat", (ws, req) => {
+  const id = crypto.randomUUID();
+  console.log("Client connected, id=", id);
+  activeConnections[id] = ws; //присвоение каждому юзеру айди
+
+  ws.on("close", () => {
+    console.log("client disconnected! id=", id);
+    delete activeConnections[id]; //удаление сессии юзера
+  });
+
+  ws.on("message", (msg) => {
+    const decodedMessage = JSON.parse(msg);
+    switch (decodedMessage.type) {
+      case "CREATE_MESSAGE":
+        Object.keys(activeConnections).forEach((connId) => {
+          const conn = activeConnections[connId];
+
+          conn.send(
+            JSON.stringify({
+              type: "NEW_MESSAGE",
+              message: decodedMessage.message,
+            }),
+          );
+        });
+        break;
+      default:
+        console.log("Unknown message", decodedMessage.type);
+    }
+    ws.send(msg);
+  });
+  console.log(activeConnections);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState("");
 
-export default App
+  const ws = useRef(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8000/chat");
+
+    ws.current.onmessage = (event) => {
+      const decodedMessage = JSON.parse(event.data);
+
+      if (decodedMessage.type === "NEW_MESSAGE") {
+        setMessages((prev) => [...prev, decodedMessage.message]);
+      }
+    };
+  }, []);
+
+  const sendMessage = () => {
+    ws.current.send(
+      JSON.stringify({
+        type: "CREATE_MESSAGE",
+        message: messageText,
+      }),
+    );
+  };
+
+  return (
+    <>
+      <p>
+        <input
+          type="text"
+          value={messageText}
+          name="messageText"
+          onChange={(e) => setMessageText(e.target.value)}
+        />
+      </p>
+      <button onClick={sendMessage}>send</button>
+
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <p>{message}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
